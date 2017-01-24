87bffca9199be1d113ff1298b0133d8e6e0a8597
Sergey Shyiko <sergey.shyiko@revjet.com>
2017-01-23 21:40:38 -0400

    added loading spinner

--- src/public/containers/App.tsx
+++ src/public/containers/App.tsx
@@ -3,16 +3,19 @@ import CSSModules from 'react-css-modules';
 import { SockscodeToolbar } from './toolbar/SockscodeToolbar';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux'
+import { Spinner, SpinnerType } from 'office-ui-fabric-react/lib/Spinner';
 import { sockscodeApp } from '../reducers/Reducers';
 import { SocketIoCodeService } from '../service/SocketIoCodeService';
+import { OfficeService } from '../service/OfficeService';
 import { createdRoom, codeChanged, remoteCodeChanged } from '../actions/Actions'
+import { css } from 'office-ui-fabric-react/lib/Utilities';
 
 interface AppProps {
 
 }
 
 interface AppState {
-
+    officeInitialized: boolean
 }
 
 const store = createStore(sockscodeApp);
@@ -24,19 +27,28 @@ socketIoCodeService.onCreateRoom((roomUuid) => {
 socketIoCodeService.onCodeChange((codeChangeSocketData) => {
     store.dispatch(remoteCodeChanged(codeChangeSocketData.code));
 });
+const officeService: OfficeService = new OfficeService();
 
 @CSSModules(styles)
 export class App extends React.Component<AppProps, AppState>{
+
+    state = {
+        officeInitialized: false
+    }
     constructor() {
         super();
+        officeService.promiseInitialize().then(() => {
+            this.setState({ officeInitialized: true });
+        });
+        //fixme catch? fixme redux
     }
 
     render() {
         return <Provider store={store}>
-            <div className={styles.container}>
-                <div>
+            <div className={css(styles.container, 'ms-Fabric', 'ms-font-m')}>
+                {this.state.officeInitialized ? <div>
                     <SockscodeToolbar />
-                </div>
+                </div> : <Spinner className={styles.spinner} type={SpinnerType.large} label='Initializing...' />}
             </div>
         </Provider>
     }
